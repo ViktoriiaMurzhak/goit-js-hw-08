ece1ee33d4ed3ff560b172dfcecc41f6abccb512
Viktoriia <viktoria.murzhak@gmail.com>
2022-09-14 15:36:21 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -41,3 +41,34 @@ function populateTextarea() {
     }
   }
 }
+
+// ! Інший варіант
+// import Throttle from 'lodash.throttle';
+// const form = document.querySelector('.feedback-form');
+// const email = form.querySelector('[name="email"]');
+// const message = form.querySelector('[name="message"]');
+// const localKey = 'feedback-form-state';
+// form.addEventListener('input', Throttle(storageFormData, 500));
+// form.addEventListener('submit', onFormSubmit);
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   // const { email, message } = event.currentTarget.elements;
+//   // вывести в консоль в1
+//   //   console.log({ email: email.value, message: message.value });
+//   // вывести в консоль в2
+//   const savedData = JSON.parse(localStorage.getItem(localKey));
+//   console.dir(savedData);
+//   localStorage.removeItem(localKey);
+//   event.currentTarget.reset();
+// }
+// function storageFormData(event) {
+//   const formValue = { email: '', message: '' };
+//   if (localStorage.getItem(localKey)) {
+//     Object.assign(formValue, JSON.parse(localStorage.getItem(localKey)));
+//   }
+//   //   console.log('name input:', event.target.name);
+//   //   console.log('value input:', event.target.value);
+//   formValue[event.target.name] = event.target.value;
+//   //   console.log(formValue);
+//   localStorage.setItem(localKey, JSON.stringify(formValue));
+// }
